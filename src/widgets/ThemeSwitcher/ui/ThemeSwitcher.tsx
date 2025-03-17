89172727b49a7464029408179bb4bfdab50d2090
Maxim <fapmit01@gmail.com>
2025-03-17 23:50:21 +0300

    fix problem with testing

--- src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
+++ src/widgets/ThemeSwitcher/ui/ThemeSwitcher.tsx
@@ -1,8 +1,8 @@
 import { classNames } from "shared/lib/classNames/classNames";
 import cls from "./ThemeSwitcher.module.scss";
 import { Theme, useTheme } from "app/providers/ThemeProvider";
-import LightIcon from "shared/assets/icons/themeLight.svg";
-import DarkIcon from "shared/assets/icons/themeDark.svg";
+import LightIcon from "shared/assets/icons/theme-light.svg";
+import DarkIcon from "shared/assets/icons/theme-dark.svg";
 import { Button, ThemeButton } from "shared/ui/Button/Button";
 
 
