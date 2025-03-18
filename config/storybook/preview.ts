bd16b50cda6f57ccaf97b7017aa5c145ed166ff8
Maxim Fetisov <95105130+FapMit@users.noreply.github.com>
2025-03-18 16:59:48 +0300

    Add files via upload

--- config/storybook/preview.ts
+++ config/storybook/preview.ts
@@ -1,8 +1,8 @@
-import 'loki/configure-react';
-import type { Preview } from '@storybook/react';
+import type { Preview } from '@storybook/react'
 import { StyleDecorator } from '../../src/shared/config/storybook/StyleDecorator/StyleDecorator';
 import { ThemeDecorator } from '../../src/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { RouterDecorator } from '../../src/shared/config/storybook/RouterDecorator/RouterDecorator';
+import { TranslationDecorator } from '../../src/shared/config/storybook/TranslationDecorator/TranslationDecorator';
 import { Theme } from '../../src/app/providers/ThemeProvider';
 
 const preview: Preview = {
@@ -17,6 +17,7 @@ const preview: Preview = {
   decorators: [
     StyleDecorator, 
     RouterDecorator,
+    TranslationDecorator, 
     ThemeDecorator(Theme.LIGHT)
   ]
 };
