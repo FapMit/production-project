34cf6d68bf1bd4ca7262c5ae728b64c0b6f0120d
Maxim <fapmit01@gmail.com>
2025-04-11 13:54:16 +0300

    update app router

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -1,6 +1,10 @@
 export { 
-  getUserAuthData 
-} from "./model/selectors/getUserAuthData"
+  getUserAuthData
+} from "./model/selectors/getUserAuthData/getUserAuthData"
+
+export { 
+  getUserInited
+} from "./model/selectors/getUserInited/getUserInited"
 
 export {
   userReducer,
