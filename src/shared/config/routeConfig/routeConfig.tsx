2957859a8e4f88d88b88091a9d3aff7a6ebcec98
Maxim <fapmit01@gmail.com>
2025-04-11 14:27:42 +0300

    add articles page

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,4 +1,6 @@
 import { AboutPage } from "pages/AboutPage";
+import { ArticleDetailsPage } from "pages/ArticleDetailsPage";
+import { ArticlesPage } from "pages/ArticlesPage";
 import { MainPage } from "pages/MainPage";
 import { NotFoundPage } from "pages/NotFoundPage";
 import { ProfilePage } from "pages/ProfilePage";
@@ -12,6 +14,8 @@ export enum AppRoutes {
   MAIN = 'main',
   ABOUT = 'about',
   PROFILE = 'profile',
+  ARTICLES = 'articles',
+  ARTICLE_DETAILS = 'article_details',
 
   // LAST
   NOT_FOUND = 'not_found',
@@ -21,27 +25,39 @@ export const RoutePath: Record<AppRoutes, string> = {
   [AppRoutes.MAIN]: '/',
   [AppRoutes.ABOUT]: '/about',
   [AppRoutes.PROFILE]: '/profile',
+  [AppRoutes.ARTICLES]: '/articles',
+  [AppRoutes.ARTICLE_DETAILS]: '/articles', // + id
   [AppRoutes.NOT_FOUND]: '*',
 };
 
 export const routeConfig: Record<AppRoutes, AppRoutesProps> = {
   [AppRoutes.MAIN]: {
     path: RoutePath[AppRoutes.MAIN],
-    element: <MainPage/>  
+    element: <MainPage />
   },
   [AppRoutes.ABOUT]: {
     path: RoutePath[AppRoutes.ABOUT],
-    element: <AboutPage/>  
+    element: <AboutPage />
   },
   [AppRoutes.PROFILE]: {
     path: RoutePath[AppRoutes.PROFILE],
-    element: <ProfilePage/>,  
+    element: <ProfilePage />,
+    authOnly: true,
+  },
+  [AppRoutes.ARTICLES]: {
+    path: RoutePath[AppRoutes.ARTICLES],
+    element: <ArticlesPage />,
+    authOnly: true,
+  },
+  [AppRoutes.ARTICLE_DETAILS]: {
+    path: `${RoutePath[AppRoutes.ARTICLE_DETAILS]}/:id`,
+    element: <ArticleDetailsPage />,
     authOnly: true,
   },
 
   // LAST
   [AppRoutes.NOT_FOUND]: {
     path: RoutePath[AppRoutes.NOT_FOUND],
-    element: <NotFoundPage/>  
+    element: <NotFoundPage />
   },
 };
--- src/widgets/Sidebar/model/items.ts
+++ src/widgets/Sidebar/model/items.ts
@@ -2,6 +2,8 @@ import { SVGProps, VFC } from "react";
 import HomeIcon from "shared/assets/icons/home.svg";
 import AboutIcon from "shared/assets/icons/about.svg";
 import ProfileIcon from "shared/assets/icons/profile.svg";
+import ArticlesIcon from "shared/assets/icons/articles.svg";
+import { RoutePath } from "shared/config/routeConfig/routeConfig";
 
 export interface SidebarItemType {
   path: string;
@@ -12,19 +14,25 @@ export interface SidebarItemType {
 
 export const SidebarItemsList: SidebarItemType[] = [
   {
-    path: "/",
+    path: RoutePath.main,
     text: "Главная",
     Icon: HomeIcon,
   },
   {
-    path: "/about",
+    path: RoutePath.about,
     text: "О нас",
     Icon: AboutIcon,
   },
   {
-    path: "/profile",
+    path: RoutePath.profile,
     text: "Профиль",
     Icon: ProfileIcon,
     authOnly: true
   },
+  {
+    path: RoutePath.articles,
+    text: "Статьи",
+    Icon: ArticlesIcon,
+    authOnly: true
+  }
 ]
