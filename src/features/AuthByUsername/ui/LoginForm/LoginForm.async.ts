c927daf7d140e6f4081e95e31f060ef6664b76fd
Maxim <fapmit01@gmail.com>
2025-05-04 21:36:07 +0300

    production

--- src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
+++ src/features/AuthByUsername/ui/LoginForm/LoginForm.async.ts
@@ -1,9 +1,3 @@
 import { lazy } from 'react';
 
 export const LoginFormAsync = lazy(() => import('./LoginForm'));
-
-// Для тестирования асинхронного компонента с искуственной задержкой загрузки
-// export const LoginFormAsync = lazy<FC<LoginFormProps>>(() => new Promise((resolve) => {
-//   // @ts-ignore
-//   setTimeout(() => resolve(import('./LoginForm')), 500);
-// }));
