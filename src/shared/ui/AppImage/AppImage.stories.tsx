723db771d33ff527f1f70a9abaaa879d99ecac4a
Maxim <fapmit01@gmail.com>
2025-06-09 14:23:46 +0300

    update main.yml for github actions

--- src/shared/ui/AppImage/AppImage.stories.tsx
+++ src/shared/ui/AppImage/AppImage.stories.tsx
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
+import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { AppImage } from './AppImage';
-import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
-import { Theme } from 'app/providers/ThemeProvider';
+import { Theme } from '@/shared/const/theme';
 
 const meta = {
   title: 'Shared/AppImage',
