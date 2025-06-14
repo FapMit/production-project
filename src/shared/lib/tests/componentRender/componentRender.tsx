59503b3affc396e03a0ece9a96495c0cd8690e0e
Maxim <fapmit01@gmail.com>
2025-06-14 16:45:01 +0300

    add test for isolated components with cypress

--- src/shared/lib/tests/componentRender/componentRender.tsx
+++ src/shared/lib/tests/componentRender/componentRender.tsx
@@ -5,28 +5,56 @@ import { ReactNode } from "react";
 import { I18nextProvider } from "react-i18next";
 import { MemoryRouter } from "react-router-dom";
 import i18nForTests from "@/shared/config/i18n/i18nForTests";
+import { Theme } from "@/shared/const/theme";
+// eslint-disable-next-line test-plugin/layer-imports
+import { ThemeProvider } from "@/app/providers/ThemeProvider"; 
+// eslint-disable-next-line test-plugin/layer-imports
+import '@/app/styles/index.scss';
 
 export interface componentRenderOptions {
   route?: string;
   initialState?: DeepPartial<StateSchema>;
   asyncReducers?: DeepPartial<ReducersMapObject<StateSchema>>;
+  theme?: Theme;
 }
 
-export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+interface TestProviderProps {
+  children: ReactNode;
+  options?: componentRenderOptions;
+}
+
+export function TestProvider(props: TestProviderProps) {
+  const {
+    children,
+    options = {},
+  } = props;
+
   const {
     route = '/',
     initialState,
-    asyncReducers
+    asyncReducers,
+    theme = Theme.LIGHT,
   } = options;
-
-  return render(
+  return (
     <MemoryRouter initialEntries={[route]}>
       <StorePorvider asyncReducers={asyncReducers}
         initialState={initialState}>
         <I18nextProvider i18n={i18nForTests}>
-          {component}
+          <ThemeProvider initialTheme={theme}>
+            <div className={`app ${theme}`}>
+              {children}
+            </div>
+          </ThemeProvider>
         </I18nextProvider>
       </StorePorvider>
     </MemoryRouter>
+  )
+}
+
+export function componentRender(component: ReactNode, options: componentRenderOptions = {}) {
+  return render(
+    <TestProvider options={options}>
+      {component}
+    </TestProvider>
   )
 }
